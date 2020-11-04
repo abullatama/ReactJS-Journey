6c9c8fb18288e84de8dffcd3309cc211e3cb1c16
abullatama <abullatama@gmail.com>
2020-11-05 03:06:15 +0700

    Soal 4

--- Tugas-Harian-Part-1/Tugas-3/index.js
+++ Tugas-Harian-Part-1/Tugas-3/index.js
@@ -35,3 +35,18 @@ console.log('Kata Kedua: ' + kataKedua);
 console.log('Kata Ketiga: ' + kataKetiga);
 console.log('Kata Keempat: ' + kataKeempat);
 console.log('Kata Kelima: ' + kataKelima);
+
+//tugas 4
+var nilai = 75;
+
+if (nilai >= 80) {
+    console.log("A");
+} else if (nilai >= 70 && nilai < 80) {
+    console.log("B");
+} else if (nilai >= 60 && nilai < 70) {
+    console.log("C");
+} else if (nilai >= 50 && nilai < 60) {
+    console.log("D");
+} else if (nilai < 50) {
+    console.log("E");
+}
