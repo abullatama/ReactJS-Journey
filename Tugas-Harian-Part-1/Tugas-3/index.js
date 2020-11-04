0263e0366ffbb9d78150b0de7c016a046cd66809
abullatama <abullatama@gmail.com>
2020-11-05 02:46:24 +0700

    Soal 2

--- Tugas-Harian-Part-1/Tugas-3/index.js
+++ Tugas-Harian-Part-1/Tugas-3/index.js
@@ -5,3 +5,18 @@ var kataKetiga = "belajar";
 var kataKeempat = "javascript";
 
 console.log(kataPertama + " " + kataKedua + " " + kataKetiga + " " + kataKeempat);
+
+// tugas 2
+var kataPertama = "1";
+var kataKedua = "2";
+var kataKetiga = "4";
+var kataKeempat = "5";
+
+var strPertama = parseInt(kataPertama);
+var strKedua = parseInt(kataKedua);
+var strKetiga = parseInt(kataKetiga);
+var strKeempat = parseInt(kataKeempat);
+
+var jumlah = strPertama + strKedua + strKetiga + strKeempat;
+
+console.log(jumlah);
