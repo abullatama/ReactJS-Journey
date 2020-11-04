a692a17913371f693e094e4d6e20db1f02b9dbdb
abullatama <abullatama@gmail.com>
2020-11-05 02:56:38 +0700

    Soal 3

--- Tugas-Harian-Part-1/Tugas-3/index.js
+++ Tugas-Harian-Part-1/Tugas-3/index.js
@@ -20,3 +20,18 @@ var strKeempat = parseInt(kataKeempat);
 var jumlah = strPertama + strKedua + strKetiga + strKeempat;
 
 console.log(jumlah);
+
+//tugas 3
+var kalimat = 'wah javascript itu keren sekali';
+
+var kataPertama = kalimat.substring(0, 3);
+var kataKedua = kalimat.substring(4, 14);
+var kataKetiga = kalimat.substring(15, 18);
+var kataKeempat = kalimat.substring(19, 24);
+var kataKelima = kalimat.substring(25, 31);
+
+console.log('Kata Pertama: ' + kataPertama);
+console.log('Kata Kedua: ' + kataKedua);
+console.log('Kata Ketiga: ' + kataKetiga);
+console.log('Kata Keempat: ' + kataKeempat);
+console.log('Kata Kelima: ' + kataKelima);
