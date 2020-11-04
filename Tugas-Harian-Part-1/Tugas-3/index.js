668f3a98a7e839838577d1c8876395f75c667ec8
abullatama <abullatama@gmail.com>
2020-11-05 02:45:11 +0700

    Soal 1

---
+++ Tugas-Harian-Part-1/Tugas-3/index.js
@@ -0,0 +1,7 @@
+// tugas 1
+var kataPertama = "saya";
+var kataKedua = "senang";
+var kataKetiga = "belajar";
+var kataKeempat = "javascript";
+
+console.log(kataPertama + " " + kataKedua + " " + kataKetiga + " " + kataKeempat);
