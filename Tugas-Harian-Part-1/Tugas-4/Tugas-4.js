fd49142b8b57f0222b77161d4c36f4d60dbef3b7
abullatama <abullatama@gmail.com>
2020-11-05 19:00:29 +0700

    Tugas-4 - Soal 2

--- Tugas-Harian-Part-1/Tugas-4/Tugas-4.js
+++ Tugas-Harian-Part-1/Tugas-4/Tugas-4.js
@@ -16,3 +16,17 @@ while (i > 0) {
     i = i - 2
 }
 console.log("")
+
+//Soal 2
+console.log("Soal 2")
+console.log("")
+for (var i = 1; i <= 20; i++) {
+    if (i % 3 == 0 && i % 2 == 1) {
+        console.log(i + " I Love Coding")
+    } else if (i % 2 == 0) {
+        console.log(i + " Berkualitas")
+    } else if (i % 2 == 1) {
+        console.log(i + " Santai")
+    }
+}
+console.log("")
