a6cc9231ae529918f67aae640571563b40c43fd5
abullatama <abullatama@gmail.com>
2020-11-05 18:56:45 +0700

    Membuat file Tugas-4.js - Soal 1

---
+++ Tugas-Harian-Part-1/Tugas-4/Tugas-4.js
@@ -0,0 +1,18 @@
+//Soal 1
+var i = 2
+console.log("")
+console.log("Soal 1")
+console.log("")
+console.log("LOOPING PERTAMA")
+while (i <= 20) {
+    console.log(i + " - I love Coding")
+    i = i + 2
+}
+
+i = 20
+console.log("LOOPING KEDUA")
+while (i > 0) {
+    console.log(i + " - I will become a frontend developer")
+    i = i - 2
+}
+console.log("")
