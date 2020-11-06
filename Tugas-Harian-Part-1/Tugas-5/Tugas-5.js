08bdf7e4ceb4da5026b7cf4224194a392af99c52
abullatama <abullatama@gmail.com>
2020-11-07 01:55:51 +0700

    Tugas-5.js - Soal 3

--- Tugas-Harian-Part-1/Tugas-5/Tugas-5.js
+++ Tugas-Harian-Part-1/Tugas-5/Tugas-5.js
@@ -20,3 +20,17 @@ var hasilKali = kalikan(num1, num2)
 console.log(hasilKali)
 console.log("")
 
+//Soal 3
+console.log("Soal 3")
+function introduce(name, age, address, hobby) {
+    return "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby + "!"
+}
+
+var name = "John"
+var age = 30
+var address = "Jalan belum jadi"
+var hobby = "Gaming"
+
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan)
+console.log("")
