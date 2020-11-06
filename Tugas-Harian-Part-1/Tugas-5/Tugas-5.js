447d5b49f8423f0698c46b7a4f0c012c7135a0cb
abullatama <abullatama@gmail.com>
2020-11-07 01:48:37 +0700

    Tugas-5.js - Soal 2

--- Tugas-Harian-Part-1/Tugas-5/Tugas-5.js
+++ Tugas-Harian-Part-1/Tugas-5/Tugas-5.js
@@ -7,4 +7,16 @@ function halo() {
 console.log(halo())
 console.log("")
 
+//Soal 2
+console.log("Soal 2")
+function kalikan(num1, num2) {
+    return num1 * num2
+}
+
+var num1 = 12
+var num2 = 4
+
+var hasilKali = kalikan(num1, num2)
+console.log(hasilKali)
+console.log("")
 
