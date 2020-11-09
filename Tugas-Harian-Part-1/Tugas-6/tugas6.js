b5605cf882fdb4828e1a7e59033a6f71c87e983d
abullatama <abullatama@gmail.com>
2020-11-09 21:25:56 +0700

    Membuat file tugas6.js dan Soal 1

---
+++ Tugas-Harian-Part-1/Tugas-6/tugas6.js
@@ -0,0 +1,13 @@
+//Soal 1
+//Keliling Lingkaran
+const kelilingLingkaran = (r) => {
+    return 2 * 3.14 * r
+}
+
+//Luas Lingkaran
+const luasLingkaran = (r) => {
+    return 3.14 * Math.pow(r, 2)
+}
+
+console.log(kelilingLingkaran(4))
+console.log(luasLingkaran(4))
