c36cf615f5ba07167527804402adeeae80c17302
abullatama <abullatama@gmail.com>
2020-11-09 22:48:00 +0700

    tugas6.js - Soal 4

--- Tugas-Harian-Part-1/Tugas-6/tugas6.js
+++ Tugas-Harian-Part-1/Tugas-6/tugas6.js
@@ -52,3 +52,19 @@ const newFunction = literal = (firstName, lastName) => {
 //Driver Code 
 newFunction("William", "Imoh").fullName()
 console.log("")
+
+//Soal 4
+console.log("Soal 4")
+console.log("")
+
+const newObject = {
+    firstName: "Harry",
+    lastName: "Potter Holt",
+    destination: "Hogwarts React Conf",
+    occupation: "Deve-wizard Avocado",
+    spell: "Vimulus Renderus!!!"
+}
+
+const { firstName, lastName, destination, occupation, spell } = newObject
+console.log(firstName, lastName, destination, occupation)
+console.log("")
