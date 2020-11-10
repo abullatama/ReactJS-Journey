4b3b8009c555cf68bb92fd44e6d89c45af32d472
abullatama <abullatama@gmail.com>
2020-11-10 21:58:42 +0700

    tugas7.js - Soal 1 Release 1

--- Tugas-Harian-Part-2/Tugas-7/tugas7.js
+++ Tugas-Harian-Part-2/Tugas-7/tugas7.js
@@ -11,6 +11,41 @@ class Animal {
 }
 var sheep = new Animal("shaun");
 
-console.log(sheep.name) // "shaun"
-console.log(sheep.legs) // 4
-console.log(sheep.cold_blooded) // false
+console.log("Release 0")
+console.log(sheep.name)
+console.log(sheep.legs)
+console.log(sheep.cold_blooded)
+console.log("")
+
+//Release 1
+class Ape extends Animal {
+    constructor(name) {
+        super(name)
+    }
+    yell() {
+        console.log("Auooo")
+    }
+}
+
+class Frog extends Animal {
+    constructor(name) {
+        super(name)
+    }
+    jump() {
+        console.log("hop hop")
+    }
+}
+
+var sungokong = new Ape("kera sakti")
+
+console.log("Release 1")
+console.log(sungokong.name)
+console.log(sungokong.legs = 2)
+console.log(sungokong.cold_blooded)
+sungokong.yell()
+console.log("")
+var kodok = new Frog("buduk")
+console.log(kodok.name)
+console.log(kodok.legs)
+console.log(kodok.cold_blooded)
+kodok.jump() // "hop hop" 
