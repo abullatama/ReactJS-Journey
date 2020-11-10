ed15dc787a2ad438bd16bd23b425196faef613f0
abullatama <abullatama@gmail.com>
2020-11-11 00:13:52 +0700

    tugas7.js - Release 1

--- Tugas-Harian-Part-1/Tugas-7/tugas7.js
+++ Tugas-Harian-Part-1/Tugas-7/tugas7.js
@@ -18,3 +18,37 @@ console.log(sheep.legs)
 console.log(sheep.cold_blooded)
 console.log("")
 
+//Release 1
+class Ape extends Animal {
+    constructor(name) {
+        super(name)
+    }
+    yell() {
+        console.log("Auooo")
+    }
+}
+
+class Frog extends Animal {
+    constructor(name) {
+        super(name)
+    }
+    jump() {
+        console.log("hop hop")
+    }
+}
+
+var sungokong = new Ape("kera sakti")
+
+console.log("----Release 1----")
+console.log(sungokong.name)
+console.log(sungokong.legs = 2)
+console.log(sungokong.cold_blooded)
+sungokong.yell()
+console.log("")
+var kodok = new Frog("buduk")
+console.log(kodok.name)
+console.log(kodok.legs)
+console.log(kodok.cold_blooded)
+kodok.jump() // "hop hop" 
+
+
