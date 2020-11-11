1b6d98325602016e65c8ca21a49d8699baef8dfd
abullatama <abullatama@gmail.com>
2020-11-11 19:06:46 +0700

    Tugas-8 - Soal 2 (Promise)

--- Tugas-Harian-Part-1/Tugas-8/index.js
+++ Tugas-Harian-Part-1/Tugas-8/index.js
@@ -4,16 +4,21 @@ var books = [
     { name: 'LOTR', timeSpent: 3000 },
     { name: 'Fidas', timeSpent: 2000 },
     { name: 'Kalkulus', timeSpent: 4000 },
-    { name: 'komik', timeSpent: 1000 }
+    { name: 'komik', timeSpent: 1000 },
+    { name: 'Koran', timeSpent: 1000 }
 ]
 
 //Soal 1
 let i = 0
 function execute(sisaWaktu) {
     readBooks(sisaWaktu, books[i], function (sisaWaktu) {
+        i++
         if (sisaWaktu > 0) {
-            i++
             execute(sisaWaktu)
+        } else {
+            readBooks(sisaWaktu, books[i], function (time) {
+                console.log(`Sisa waktu saya ${time}. Saya tidak dapat melanjutkan membaca ${books[i].name}.`)
+            })
         }
     })
 }
--- Tugas-Harian-Part-1/Tugas-8/index2.js
+++ Tugas-Harian-Part-1/Tugas-8/index2.js
@@ -3,7 +3,20 @@ var readBooksPromise = require('./promise.js')
 var books = [
     { name: 'LOTR', timeSpent: 3000 },
     { name: 'Fidas', timeSpent: 2000 },
-    { name: 'Kalkulus', timeSpent: 4000 }
+    { name: 'Kalkulus', timeSpent: 4000 },
+    { name: 'komik', timeSpent: 3000 }
 ]
 
-// Lanjutkan code untuk menjalankan function readBooksPromise 
+let i = 0
+function executePromise(time) {
+    readBooksPromise(time, books[i])
+        .then(fullfilled => {
+            i++
+            executePromise(fullfilled)
+        })
+        .catch(error => {
+            console.log(`Waktu saya ${error} untuk melanjutkan membaca ${books[i].name}.`)
+        })
+}
+
+executePromise(10000)
