8be7ae612a4ec79af7c4c6cfd3733595ddfb3c44
abullatama <abullatama@gmail.com>
2020-11-12 22:20:22 +0700

    Merubah tampilan App.js pada folder src dengan memodifikasi css pada App.css

--- Tugas-Harian-Part-2/Tugas-9/my-app/src/App.js
+++ Tugas-Harian-Part-2/Tugas-9/my-app/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-content">
+          <h1>Form Pembelian Buah</h1>
+          <form>
+            <div className="flex-row">
+              <div className="flex-col">
+                <label><b>Nama Pelanggan</b></label>
+              </div>
+              <div className="flex-col-input">
+                <input type="text" size="22"></input>
+              </div>
+            </div>
+            <div className="flex-row">
+              <div className="flex-col">
+                <label><b>Daftar Item</b></label>
+              </div>
+              <div className="flex-col-input">
+                <input type="checkbox"></input>
+                <label><b>Semangka</b></label><br />
+                <input type="checkbox"></input>
+                <label><b>Jeruk</b></label><br />
+                <input type="checkbox"></input>
+                <label><b>Nanas</b></label><br />
+                <input type="checkbox"></input>
+                <label><b>Salak</b></label><br />
+                <input type="checkbox"></input>
+                <label><b>Anggur</b></label><br />
+              </div>
+            </div>
+            <button type="submit">Kirim</button>
+          </form>
+        </div>
       </header>
-    </div>
+    </div >
   );
 }
 
